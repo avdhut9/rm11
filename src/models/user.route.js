23ad964938d6e8fd5a8644d08647472efff46203
Avadhut <avdhutshinde22@gmail.com>
2023-01-28 13:28:17 +0530

    add

--- src/models/user.route.js
+++ src/models/user.route.js
@@ -4,14 +4,14 @@ const jwt=require("jsonwebtoken")
 app.post("/",async(req,res)=>{
     const{refreshtoken}=req.headers
     if(!refreshtoken){
-        res.send("not authorized")
+        res.send("refrsh token unauthorized")
     }try{
 const verify=jwt.verify(refreshtoken,"avdhut$123")
 
     const token=jwt.sign({_id:verify._id,email:verify.email},"avdhut@0511",{expiresIn:"7 days"})
 return res.send({token})
     }catch(e){
-return res.send(e.message)
+return res.send("refresh token expired")
     }
 })
 module.exports=app
